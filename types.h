c5e5886faeee02657ec8f7382052824ea6e86a94
Johannes Kulick <kulick@hildensia.de>
2014-01-06 00:52:12 +0100

    some types I need here and there

---
+++ types.h
@@ -0,0 +1,11 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#include <cstdint>
+#include <map>
+#include <string>
+
+typedef int64_t pos_t;
+typedef std::map<std::string, std::string> metadata_t;
+
+#endif
