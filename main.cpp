13250998a71a15925c7224bd832a6e0756ffc304
Johannes Kulick <kulick@hildensia.de>
2014-01-06 10:54:45 +0100

    parse audio file from argument

--- main.cpp
+++ main.cpp
@@ -2,13 +2,21 @@
 #include "player.h"
 #include "curses_interface.h"
 #include <string>
+#include <iostream>
 extern "C" {
 #include "libavcodec/avcodec.h"
 }
 int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cout << "Usage: playalong <AUDIO_FILE>" << std::endl;
+    exit(0);  
+  }
+  std::string file(argv[1]);
+  
   av_register_all();
   av_log_set_level(AV_LOG_QUIET);
-  SoundStream song(std::string("test.mp3"));  
+
+  SoundStream song(file);  
 
   Player player(song);
   player.play();
