16135ff3b7199b9c7b222b35dd811dbf3747c43e
Johannes Kulick <kulick@hildensia.de>
2014-02-11 13:55:48 +0100

    some types for the phase vocoder

--- types.h
+++ types.h
@@ -4,8 +4,14 @@
 #include <cstdint>
 #include <map>
 #include <string>
+#include <functional>
+#include <vector>
+#include <fftw3.h>
 
 typedef int64_t pos_t;
 typedef std::map<std::string, std::string> metadata_t;
+typedef std::function<double(const int, const int)> window_fct_t;
+typedef std::vector<fftw_complex*> stft_t;
+typedef std::vector<double> istft_t;
 
 #endif
