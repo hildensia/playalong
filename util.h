05f6c4c27ebd330ca25e6695477e38f4d44dac10
Johannes Kulick <kulick@hildensia.de>
2014-02-11 13:56:13 +0100

    a scaling function, for fftw returns unscaled data

--- util.h
+++ util.h
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <map>
+#include <algorithm>
 
 struct AVDictionary;
 
@@ -19,4 +20,15 @@ void ms_to_readable(const int ms, int& h, int& m, int& s, int& mms);
 std::string ms_to_readable(const int ms);
 
 metadata_t av_dict_to_metadata(AVDictionary* dict);
+
+void print_complex(fftw_complex* cpx, size_t size);
+
+template<class T>
+std::vector<T> scale(std::vector<T> v) {
+  auto m = std::max_element(v.begin(), v.end());
+  std::vector<T> r(v.size());
+  for (uint i=0; i<v.size(); ++i)
+    r[i] = v[i]/(*m);
+  return r;
+}
 #endif
